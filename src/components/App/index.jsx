e51c0a0be0e3deff3c95035e0997f7a7f55c8ecd
artginzburg <art.ginzburg@ya.ru>
2021-09-25 16:38:57 +0300

    add NotFound page

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,12 +2,14 @@ import { Route, Switch } from 'react-router';
 
 import Main from '../Main';
 import Movies from '../Movies';
+import NotFound from '../NotFound';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Main} />
       <Route path="/movies" component={Movies} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
