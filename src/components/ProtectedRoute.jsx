2af848b8ecfe78ba8f66debdfd4aa7645b6d7ccd
artginzburg <art.ginzburg@ya.ru>
2021-10-07 06:07:04 +0300

    redirect unauthorized users to /

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,6 +3,6 @@ import { Route, Redirect } from 'react-router-dom';
 import { paths } from '../utils/constants';
 
 const ProtectedRoute = (props) => (
-  <Route path={props.path}>{props.loggedIn ? props.children : <Redirect to={paths.login} />}</Route>
+  <Route path={props.path}>{props.loggedIn ? props.children : <Redirect to={paths.main} />}</Route>
 );
 export default ProtectedRoute;
